b9825878bf1605141f9dbb8f223b258c7eb03a78
Fabrício M. Damasceno <fabricio_wm@hotmail.com>
2017-10-18 23:46:37 -0200

    Publicando projeto AspNetMVC

---
+++ Models/City.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Models
+{
+    public class City
+    {
+        [Key]
+        [Display(Name = "Cidade ID")]
+        public int CityId { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
+        [Display(Name = "Cidade")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
+        [Display(Name = "Departamento")]
+        [Range(1, double.MaxValue, ErrorMessage ="Selecione um Departamento")]
+        public int DepartamentsId { get; set; }
+
+        public virtual Departaments Departament { get; set; }
+    }
+}
---
+++ Models/Departaments.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ECommerce.Models
+{
+    public class Departaments
+    {
+        [Key]
+        [Display(Name = "Departamento")]
+        public int DepartamentsId { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
+        [Display(Name = "Nome")]
+        public string Name { get; set; }
+
+        public virtual ICollection<City> Cities { get; set; }
+    }
+}
